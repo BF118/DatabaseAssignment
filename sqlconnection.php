195178a38afdcd4a4b396d94f499df101088fd62
Audrey Smith <audcsm@gmail.com>
2022-05-13 11:48:31 +0100

    <mod> Made queries more complex

--- sqlconnection.php
+++ sqlconnection.php
@@ -17,33 +17,34 @@ $connection = new PDO($dsn, $user, $pass);
 echo "<h2>Inserting into DEPT table:</h2>";
 
 // Insert to DEPT
-/* $sql = "INSERT INTO DEPT(DEPTNO, DNAME, LOC) VALUES (50, 'HUMAN-RESOURCES', 'LONDON')";
+$sql = "INSERT INTO DEPT(DEPTNO, DNAME, LOC) VALUES (50, 'HUMAN-RESOURCES', 'LONDON')";
 $result = $connection->prepare($sql);
 $result->execute();
 if ($result) {
-	echo "New values inserted into dept";
+	echo "<p>New values inserted into dept table.</p>";
 }
 else {
-	echo "Error inserting new values into dept";
-} */
+	echo "<p>Error inserting new values into dept table.</p>";
+} 
 
 echo "<h2>Inserting into EMP table:</h2>";
 
 // Insert to EMP
-/* $sql = "INSERT INTO EMP(EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO) VALUES (7741, 'JONES', 'SUPERVISOR', 7839, '1985-11-04', 3000.00, NULL, 50)";
+$sql = "INSERT INTO EMP(EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO) VALUES (7741, 'JONES', 'SUPERVISOR', 7839, '1985-11-04', 3000.00, NULL, 50)";
 $result = $connection->prepare($sql);
 $result->execute();
 if ($result) {
-	echo "New values inserted into emp";
+	echo "<p>New values inserted into emp table</p>.";
 }
 else {
-	echo "Error inserting new values into emp";
-} */
+	echo "<p>Error inserting new values into emp table</p>.";
+} 
 
 echo "<h2>Query 1:</h2>";
+echo "<h3>Name all employees who work in sales.</h3>";
 
 // Query 1
-$query1 = "SELECT ENAME AS name, JOB AS job FROM EMP WHERE DEPTNO=30";
+$query1 = "SELECT ENAME AS name, JOB AS job FROM EMP E INNER JOIN DEPT D ON D.DEPTNO=E.DEPTNO WHERE D.DNAME='SALES'";
 $result1 = $connection->prepare($query1);
 $result1->execute();
 while ($line = $result1->fetch(PDO::FETCH_ASSOC)) {
@@ -51,6 +52,7 @@ while ($line = $result1->fetch(PDO::FETCH_ASSOC)) {
 }
 
 echo "<h2>Query 2:</h2>";
+echo "<h3>Show employees' names and employee numbers who have a salary over £2000.</h3>";
 
 // Query 2
 $query2 = "SELECT EMPNO AS empno, ENAME AS name, JOB AS job, SAL AS salary FROM EMP WHERE SAL>2000";
@@ -61,13 +63,14 @@ while ($line = $result2->fetch(PDO::FETCH_ASSOC)) {
 }
 
 echo "<h2>Query 3:</h2>";
+echo "<h3>How many employees work in the Dallas department?</h3>";
 
 // Query 3
-$query3 = "SELECT E.ENAME AS name, D.LOC AS location FROM EMP E INNER JOIN DEPT D ON D.DEPTNO=E.DEPTNO";
+$query3 = "SELECT COUNT(E.ENAME) AS name, D.LOC AS location FROM EMP E INNER JOIN DEPT D ON D.DEPTNO=E.DEPTNO WHERE D.LOC='DALLAS'";
 $result3 = $connection->prepare($query3);
 $result3->execute();
 while ($line = $result3->fetch(PDO::FETCH_ASSOC)) {
-	echo "<p>" . $line['name'] . " is based out of " . $line['location'] . "</p>";
+	echo "<p>" . $line['name'] . " employees work in " . $line['location'] . "</p>";
 }
 
 // Disconnect
